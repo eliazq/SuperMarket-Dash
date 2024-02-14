880b86ae41063b69ddc1d018ca64b7202130276c
Elias Heijari <69460338+elkkuzq@users.noreply.github.com>
2024-02-14 09:36:29 +0200

    ShoppingCart Movement Ready
    TODO: Platform movement, so looks like player is moving forward

---
+++ Assets/Scripts/ShoppingCartMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ShoppingCartMovement : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    Vector3 velocity = new Vector3(0, 1, 0);
+    [Header("References")]
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Animator animator;
+    [Header("Movement Settings")]
+    //[SerializeField] private float runSpeed = 8f;
+    [SerializeField] private float jumpForce = 10f;
+
+    // Ground Check
+    bool isGrounded;
+    private float groundCheckRaidus = 0.3f;
+    bool jump = false; // if true, jump force will be added
+
+    private void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRaidus, groundLayer);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            jump = true;
+        if (!isGrounded) jump = false;
+    }
+    private void FixedUpdate() {
+        velocity = new Vector3(velocity.x, rigidbody.velocity.y, velocity.y);
+        if (jump) Jump();
+        animator.SetFloat("VelocityY", velocity.y);
+        //rigidbody.velocity = velocity + (Vector3.forward * runSpeed);
+        rigidbody.velocity = velocity;
+    }
+
+    private void Jump()
+    {
+        velocity.y = jumpForce;
+    }
+}
